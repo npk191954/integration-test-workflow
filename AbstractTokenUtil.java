93981a5620e5a57fc3cf28cf3a3ae57854fafbff
dailianjie <371660929@qq.com>
2022-12-15 20:05:11 +0800

    1.Update 添加AbstractTokenUtil抽象类。

---
+++ AbstractTokenUtil.java
@@ -0,0 +1,15 @@
+package util;
+
+/**
+ * @author dailj
+ * @date 2022/12/15 15:07
+ */
+public abstract class AbstractTokenUtil {
+    
+    /**
+     * 生成token
+     * @return token
+     */
+    abstract String generateToken();
+    
+}
